5d1d1663aea8b9144a5449033ac388e23b3ea620
EnriqueTests <enriquenada@outlook.es>
2021-07-06 18:33:41 -0500

    unknow

--- dist/main.js
+++ dist/main.js
@@ -1,4 +1,6 @@
-var doSomething = function () {
-    console.log("doSomething");
-};
-var foo = undefined;
+var vAny = 10;
+var vUnknow = 10;
+var s1 = vAny;
+var s2 = vUnknow;
+var pageNumber = "1";
+var numericPageNumber = pageNumber;
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
-const doSomething = (): void => {
-    console.log("doSomething");
-};
+let vAny: any = 10;
+let vUnknow: unknow = 10;
 
-let foo: void = undefined;
+let s1: string = vAny;
+let s2: string = vUnknow as string;
+
+let pageNumber: string = "1";
+let numericPageNumber: number = (pageNumber as unknow) as number;
