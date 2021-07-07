137732e81bc02eb907829e26e790f670a1c80669
EnriqueTests <enriquenada@outlook.es>
2021-07-07 13:27:46 -0500

    Clases

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,39 @@
-const someElement = document.querySelector(".foo");
+interface UserInterface {
+    getFullName(): string;
+}
 
-someElement.addEventListener("blur", event => {
-    const target = event.target as HTMLInputElement;
-    console.log("event", target.value);
-});
+class User implements UserInterface{
+    firstName: string;
+    lastName: string;
+    readonly unchangableName: string;
+    static readonly maxAge = 50;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.unchangableName = firstName;
+    }
+
+    getFullName(): string {
+        return this.firstName + " " + this.lastName;
+    }
+}
+
+class Admin extends User {
+    private editor: string;
+
+    setEditor(editor: string): void {
+        this.editor = editor;
+    }
+
+    getEditor(): string {
+        return this.editor;
+    }
+}
+
+const user = new User("Monster", "lessons");
+console.log(user.firstName);
+console.log(User.maxAge);
+
+const admin = new Admin('Foo', 'Bar');
+console.log(admin);
